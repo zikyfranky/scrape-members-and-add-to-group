d642a95e0f2ac36770e1893ab0723deb7e33fba2
Isaac Frank <codingwithisaac@gmail.com>
2021-08-27 12:41:47 +0100

    feat: update gitignore and write script for fetching members of group

---
+++ get_users.py
@@ -0,0 +1,79 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.errors.rpcerrorlist import ChannelPrivateError
+import csv
+import secrets
+
+client = TelegramClient(secrets.phone, secrets.api_id, secrets.api_hash)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(secrets.phone)
+    client.sign_in(secrets.phone, input('Enter the code: '))
+
+
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+ 
+# List groups user is a member of 
+result = client(
+    GetDialogsRequest(
+        offset_date=last_date,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=chunk_size,
+        hash = 0
+    )
+)
+# Add the groups to an array
+chats.extend(result.chats)
+
+# Filter the groups and get just SUPERGROUPS
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
+
+print('Choose a group to scrape members from:\n')
+i=0
+for g in groups:
+    print(str(i) + '- ' + g.title)
+    i+=1
+
+# Get group by index
+g_index = input("Enter a Number: ")
+target_group=groups[int(g_index)]
+
+
+print('\nFetching Members...')
+all_participants = []
+try:
+    all_participants = client.get_participants(target_group, aggressive=True)
+except ChannelPrivateError:
+    print('Group is private, ending')
+print('Saving In file...')
+file_name = target_group.title.lower().replace(' ', '_')
+with open(file_name+".csv","w",encoding='UTF-8') as f:
+    writer = csv.writer(f,delimiter=",",lineterminator="\n")
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+    for user in all_participants:
+        if user.username:
+            username= user.username
+        else:
+            username= ""
+        if user.first_name:
+            first_name= user.first_name
+        else:
+            first_name= ""
+        if user.last_name:
+            last_name= user.last_name
+        else:
+            last_name= ""
+        name= (first_name + ' ' + last_name).strip()
+        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
+print('Members scraped successfully.')
